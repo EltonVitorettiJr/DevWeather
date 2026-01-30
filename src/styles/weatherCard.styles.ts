d274562eb1e9c0ea1160e3abe6109b1cbf4b9ce7
EltonVitorettiJr <eltonvitorettijr@gmail.com>
2026-01-30 18:22:14 -0300

    Corrigido bug de ícono invisível

--- src/styles/weatherCard.styles.ts
+++ src/styles/weatherCard.styles.ts
@@ -37,6 +37,7 @@ export const weatherCardStyles = StyleSheet.create({
     width: 100,
     height: 100,
     marginBottom: spacing.sm,
+    filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
   },
 
   temperature: {
